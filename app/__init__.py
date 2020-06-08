8ed8a1462ca90d75509e754a3aeeb7cf6922afb4
Justin Maldonado <justin.maldonado@stonybrook.edu>
2020-06-08 10:38:34 -0400

    Finalized email settings for production

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,11 @@ from . import constants
 app = Flask(__name__)
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'saintschurchtest@gmail.com'
-app.config['MAIL_PASSWORD'] = 's@1ntschurch'
+app.config['MAIL_USERNAME'] = 'saintschurchnoreply@gmail.com'
+app.config['MAIL_PASSWORD'] = 'kyxqgvexqdoaejsa'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_DEFAULT_SENDER'] = 'saintschurchtest@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'sainstchurchnoreply@gmail.com'
 
 # app.config['SERVER_NAME'] = 'localhost:5000'
 
